acd9fb5c76e54f45f2e96d8d16ad29546e91752a
jon rumsey <jon.rumsey@gmail.com>
2022-04-08 11:25:20 -0700

    arrow function without constructor breaks js.

--- app.js
+++ app.js
@@ -156,18 +156,23 @@ let message = (name) => `Hello, ${name}!`;
 // TODO: Uncomment the following line of code to see the output in the browser console
 console.log(message('Allie'));
 
-
-let Student = function(name, age, hometown) {
+let Student = (name, age, hometown) => {
   this.name = name;
   this.age = age;
   this.hometown = hometown;
 };
 
+// let Student = function(name, age, hometown) {
+//   this.name = name;
+//   this.age = age;
+//   this.hometown = hometown;
+// };
+
 let joe = new Student('Joe', 'Schmoe', 100);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this code to break!
-// console.log(joe);
+console.log(joe);
 
 // TODO: After viewing the previous console.log(), return the code to a working state.
 
